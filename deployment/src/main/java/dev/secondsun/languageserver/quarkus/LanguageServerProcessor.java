4fd9212f3c0109de293a3ff2a682261c7265cde1
Summers Pittman <secondsun@gmail.com>
2019-10-17 19:23:41 -0400

    Hotreplacement config and removing broken code

--- deployment/src/main/java/dev/secondsun/languageserver/quarkus/LanguageServerProcessor.java
+++ deployment/src/main/java/dev/secondsun/languageserver/quarkus/LanguageServerProcessor.java
@@ -1,15 +1,7 @@
 package dev.secondsun.languageserver.quarkus;
 
-import io.quarkus.arc.deployment.BeanContainerListenerBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.annotations.ExecutionTime;
-import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
-import io.quarkus.deployment.builditem.LaunchModeBuildItem;
-
-import java.util.Properties;
-
-import static io.quarkus.runtime.LaunchMode.DEVELOPMENT;
 
 public class LanguageServerProcessor {
     private static final String FEATURE_NAME = "language-server-java";
@@ -19,15 +11,5 @@ public class LanguageServerProcessor {
         return new FeatureBuildItem(FEATURE_NAME);
     }
 
-    @BuildStep
-    @Record(ExecutionTime.STATIC_INIT)
-    BeanContainerListenerBuildItem setupHotReplacement(LaunchModeBuildItem launchMode) {
-        Properties properties = new Properties();
-
-        if (launchMode.getLaunchMode() == DEVELOPMENT) {
-            String interceptorConfig = HotReplacementInterceptor.class.getName();
-
-        }
-    }
 
 }
